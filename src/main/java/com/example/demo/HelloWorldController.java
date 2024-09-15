cec4553122ee434c20c643abebc51c54d0540fa2
Fuyuki Malahom <fuyuki325@gmail.com>
2024-09-15 04:35:58 -0700

    first commit

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/ok")
+    public String ok() {
+        return "ok";
+    }
+
+}
